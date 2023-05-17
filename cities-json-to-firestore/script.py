2600d8c8ca9dbede225944b88753c247a6ba31c6
kimhokyung <ghrud92@gmail.com>
2023-05-17 18:49:40 +0900

    (EDFJ-92) 학원 정보 저장하는 스크립트 추가

--- cities-json-to-firestore/script.py
+++ cities-json-to-firestore/script.py
@@ -7,6 +7,14 @@ CITIES_JSON_FILE_PATH = 'cities-csv-to-json/cities.json'
 FIREBASE_SERVICE_ACCOUNT_PATH = 'credentials/firebase/ballet-for-all-project-9365caabf1ba.json'
 
 
+def initialize_firebase():
+    try:
+        app = firebase_admin.get_app()
+    except ValueError as e:
+        cred = credentials.Certificate(FIREBASE_SERVICE_ACCOUNT_PATH)
+        firebase_admin.initialize_app(cred)
+
+
 def read_cities_from_json():
     with open(CITIES_JSON_FILE_PATH, 'r', encoding='utf-8') as jsonfile:
         json_data = json.load(jsonfile)
@@ -14,14 +22,12 @@ def read_cities_from_json():
 
 
 def save_to_firestore(data):
-    cred = credentials.Certificate(FIREBASE_SERVICE_ACCOUNT_PATH)
-    app = firebase_admin.initialize_app(cred)
     db = firestore.client()
-
     db.collection(u'location').document(u'cities').set({'cities': data})
 
 
 def main():
+    initialize_firebase()
     cities = read_cities_from_json()
     save_to_firestore(cities)
 
