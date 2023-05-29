500c93222d88032156eac0dc4a233a714949756d
kimhokyung <ghrud92@gmail.com>
2023-05-29 13:27:57 +0900

    (EDFJ-101) Schedule에 teacherName 필드 추가

--- academies-json-to-firestore/model/schedule.py
+++ academies-json-to-firestore/model/schedule.py
@@ -2,12 +2,14 @@ class Schedule(object):
     __START_TIME = u'startTime'
     __DURATION_IN_MINUTES = u'durationInMinutes'
     __DAY = u'day'
+    __TEACHER_NAME = u'teacherName'
     __REQUIRED_KEYS = [__START_TIME, __DURATION_IN_MINUTES, __DAY]
 
-    def __init__(self, startTime, durationInMinutes, day):
+    def __init__(self, startTime, durationInMinutes, day, teacherName):
         self.startTime = startTime
         self.durationInMinutes = int(durationInMinutes)
         self.day = day
+        self.teacherName = teacherName
 
     @staticmethod
     def from_dict(source):
@@ -19,13 +21,15 @@ class Schedule(object):
 
         return Schedule(source[Schedule.__START_TIME],
                         source[Schedule.__DURATION_IN_MINUTES],
-                        source[Schedule.__DAY])
+                        source[Schedule.__DAY],
+                        source[Schedule.__TEACHER_NAME])
 
     def to_dict(self):
         schedule = {
             Schedule.__START_TIME: self.startTime,
             Schedule.__DURATION_IN_MINUTES: self.durationInMinutes,
-            Schedule.__DAY: self.day
+            Schedule.__DAY: self.day,
+            Schedule.__TEACHER_NAME: self.teacherName
         }
         return schedule
 
@@ -36,4 +40,5 @@ class Schedule(object):
         return (f'Schedule('
                 f'startTime={self.startTime}, '
                 f'durationInMinutes={self.durationInMinutes}, '
-                f'day={self.day})')
+                f'day={self.day}, '
+                f'teacherName={self.teacherName})')
